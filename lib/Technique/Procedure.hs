7db64d79c38b3e05a00566ba503797aaf921a5bb
Andrew Cowie <andrew@operationaldynamics.com>
2015-07-03 16:23:47 +1000

    Trivial monad implementation
    No-op monad to permit prototyping.

--- lib/Technique/Procedure.hs
+++ lib/Technique/Procedure.hs
@@ -17,7 +17,42 @@
 -- contacted through http://research.operationaldynamics.com/
 --
 
-{-# LANGUAGE OverloadedStrings #-}
-
 module Technique.Procedure where
+
+import Control.Monad
+import Control.Shell
+
+--
+-- A procedure is its dependencies; it's current action IS A
+-- DEPENDENCY
+--
+
+{-
+newtype Procedure a = Procedure a
+    deriving (Functor, Monad)
+-}
+
+data Procedure a = Procedure {
+    unwrap :: a
+}
+
+instance Functor Procedure where
+    fmap f (Procedure x) = Procedure (f x)
+
+instance Applicative Procedure where
+    pure  = return
+    (<*>) = ap
+
+instance Monad Procedure where
+    return x            = Procedure x
+    (Procedure x) >>= f = f x
+
+
+runProcedure :: Procedure a -> IO a
+runProcedure = return . unwrap
+
+data Action a
+    = Empty
+    | Process (Shell a)
+
 
