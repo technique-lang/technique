ebdefd49a5d27144096de53212984ca46bffb6a2
Andrew Cowie <andrew@operationaldynamics.com>
2019-09-11 23:45:52 +1000

    Remove unnecessary files and license headers

--- lib/Technique/Procedure.hs
+++ lib/Technique/Procedure.hs
@@ -1,26 +1,6 @@
---
--- Procedures
---
--- Copyright © 2012-2015 Operational Dynamics Consulting, Pty Ltd and Others
---
--- The code in this file, and the program it is a part of, is made available
--- to you by its authors as open source software: you can redistribute it
--- and/or modify it under the terms of the GNU General Public License version
--- 2 ("GPL") as published by the Free Software Foundation.
---
--- This program is distributed in the hope that it will be useful, but WITHOUT
--- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
--- FITNESS FOR A PARTICULAR PURPOSE. See the GPL for more details.
---
--- You should have received a copy of the GPL along with this program. If not,
--- see http://www.gnu.org/licenses/. The authors of this program may be
--- contacted through http://research.operationaldynamics.com/
---
-
 module Technique.Procedure where
 
 import Control.Monad
-import Control.Shell
 
 --
 -- A procedure is its dependencies; it's current action IS A
@@ -53,6 +33,5 @@ runProcedure = return . unwrap
 
 data Action a
     = Empty
-    | Process (Shell a)
 
 
--- tests/Snippet.hs
+++ tests/Snippet.hs
@@ -1,13 +1,3 @@
---
--- Procedures
---
--- Copyright © 2012-2013 Operational Dynamics Consulting, Pty Ltd
---
--- The code in this file, and the program it is a part of, is made
--- available to you by its authors as open source software: you can
--- redistribute it and/or modify it under a BSD licence.
---
-
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -fno-warn-unused-imports #-}
 
