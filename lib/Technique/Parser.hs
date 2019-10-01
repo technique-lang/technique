bfebb8435e64a4361ee5b50c8061d2927b001f0b
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-01 23:23:32 +1000

    Add parser for SPDX Header line

--- lib/Technique/Parser.hs
+++ lib/Technique/Parser.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Technique.Parser where
 
 import Control.Monad
+import Control.Monad.Combinators
 import Data.Void (Void)
 import Data.Text (Text)
 import Text.Megaparsec
@@ -23,11 +26,30 @@ parseMagicLine = do
     void newline
     return v
 
+parseSpdxLine :: Parser (Text,Maybe Text)
+parseSpdxLine = do
+    void (char '!') <?> "second line to begin with ! character"
+    void spaceChar <?> "a space character"
+
+    license <- takeWhile1P (Just "software license description (ie an SPDX-Licence-Header value)") (\c -> not (c == ',' || c == '\n'))
+
+    copyright <- optional $ do
+        void (char ',') <?> "a comma"
+        hidden $ skipMany (spaceChar <?> "a space character")
+        void (char '©') <|> void (string "(c)")
+        void (spaceChar <?> "a space character")
+        takeWhile1P (Just "a copyright declaration") (/= '\n')
+
+    void newline
+    return (license,copyright)
+
+
 type AbstractSyntaxTree = ()    -- FIXME
 
-parseBookfile :: Parser AbstractSyntaxTree
-parseBookfile = do
-    v <- parseMagicLine
-    unless (v == __VERSION__) (fail ("currently the only recognized language version is v" ++ show __VERSION__))
+parseProcfileHeader :: Parser AbstractSyntaxTree
+parseProcfileHeader = do
+    version <- parseMagicLine
+    unless (version == __VERSION__) (fail ("currently the only recognized language version is v" ++ show __VERSION__))
+    _ <- parseSpdxLine
 
     return ()
--- tests/CheckSkeletonParser.hs
+++ tests/CheckSkeletonParser.hs
@@ -15,10 +15,10 @@ import Technique.Parser
 
 checkSkeletonParser :: Spec
 checkSkeletonParser = do
-    describe "Parse bookfile format" $ do
-        it "Correctly parses a complete first line" $ do
+    describe "Parse procfile header" $ do
+        it "Correctly parses a complete magic line" $ do
             parseMaybe parseMagicLine "% technique v2\n" `shouldBe` Just 2
-        it "Errors if first line has incorrect syntax" $ do
+        it "Errors if magic line has incorrect syntax" $ do
             parseMaybe parseMagicLine "%\n" `shouldBe` Nothing
             parseMaybe parseMagicLine "%technique\n" `shouldBe` Nothing
             parseMaybe parseMagicLine "% technique\n" `shouldBe` Nothing
@@ -27,7 +27,20 @@ checkSkeletonParser = do
             parseMaybe parseMagicLine "% technique  v2\n" `shouldBe` Nothing
             parseMaybe parseMagicLine "% technique v2 asdf\n" `shouldBe` Nothing
 
-        it "Correctly parses a complete technique program" $ do
-            parseMaybe parseBookfile [quote|
+        it "Correctly parses an SPDX header line" $ do
+            parseMaybe parseSpdxLine "! BSD-3-Clause\n" `shouldBe` Just ("BSD-3-Clause",Nothing)
+        it "Correctly parses an SPDX header line with Copyright" $ do
+            parseMaybe parseSpdxLine "! BSD-3-Clause, (c) 2019 Kermit le Frog\n" `shouldBe` Just ("BSD-3-Clause",Just "2019 Kermit le Frog")
+        it "Errors if SPDX line has incorrect syntax" $ do
+            parseMaybe parseSpdxLine "!\n" `shouldBe` Nothing
+            parseMaybe parseMagicLine "!,\n" `shouldBe` Nothing
+            parseMaybe parseMagicLine "! Public-Domain,\n" `shouldBe` Nothing
+            parseMaybe parseMagicLine "! Public-Domain, (\n" `shouldBe` Nothing
+            parseMaybe parseMagicLine "! Public-Domain, (c)\n" `shouldBe` Nothing
+            parseMaybe parseMagicLine "! Public-Domain, (c) \n" `shouldBe` Nothing
+
+        it "Correctly parses a complete technique program header" $ do
+            parseMaybe parseProcfileHeader [quote|
 % technique v0
+! BSD-3-Clause
             |] `shouldBe` Just ()
