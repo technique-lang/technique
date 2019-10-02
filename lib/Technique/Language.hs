0ad47e58a5c499657af28678cd5bc46e1ac425dd
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-03 07:43:40 +1100

    Move inital tagless code to Technique.Evaluator stub
    This started with work embeddeing an encoding in the initial or final
    tagless style after Oleg Kiselyov, but evolved to a pure abstract syntax
    tree as a parsing target and feed to pretty printing. Move the evaluator
    stubs out to a scratch pad file and leaving Technique.Language for the
    AST.

--- lib/Technique/Language.hs
+++ lib/Technique/Language.hs
@@ -14,39 +14,14 @@ data Name where
 -- newlines, perhaps.
 type Label = Rope
 
--- TODO *not* Maybe! If we have two option types here then there are four
--- possibilities we need to cater for [(Nothing,Nothing), ... (Just,Just)]
--- which means there are four semantics for a given table row. Hm.
-
--- TODO there's an ambiguity here between the type of a table and the type
--- of an individual quantity. Which is "type"?
-
-{-
-data Value = Value
-    { valueType :: Type 
-    , valueLabel :: Maybe Label
-    , valueData :: Maybe Quantity
-    }
--}
 data Role
     = Any
     | Role Rope
 
 data Markdown = Markdown Rope
 
-{-
-data Expression b where
-    Binding :: Variable b -> Expression a -> Expression b
-    Comment :: Rope -> Expression ()
-    Declaration :: (a -> b) -> Expression (a -> b)
-    Application :: Expression (a -> b) -> Expression a -> Expression b 
-    Attribute :: Role -> Expression a -> Expression a
--}
-
-
 data Type = Type
     { typeName :: Rope
---  , typeInternal :: t
     }
 
 data Procedure = Procedure
@@ -86,24 +61,3 @@ data Binding where
 
 data Operator where
     Operator :: Rope -> Operator
-
-
-{-
--- aka apply
-execute :: Procedure -> Value -> b
-execute proc value =
-  let
-    body = procedureBlock proc
-  in
-    evaluate body
-
-type Context = Map String Value -- ?
-
-
-evaluate = undefined
-
-evaluate :: Context -> Expression -> Value
-evaluate e = case e of
-    Comment _ -> ()
-    _ -> undefined
--}
