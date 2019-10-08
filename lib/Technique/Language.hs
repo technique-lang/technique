fdbee1ebb6761b073c1dbbceb382fd1c10edb0d4
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-08 21:52:15 +1100

    Add more stub types exploring statements and expressions

--- lib/Technique/Language.hs
+++ lib/Technique/Language.hs
@@ -2,22 +2,29 @@
 
 module Technique.Language where
 
+import Core.Data.Structures
 import Core.Text.Rope
 
-data Variable a
-    = Constant a
-    | Future a
+data Variable b
+    = Constant b
+    | Future b
 
-data Role = Role Rope
+data Role
+    = None
+    | Any
+    | Role Rope
 
 data Markdown = Markdown Rope
 
-data Expression a where
-    Bind :: Variable a -> Expression a -> Expression a
+{-
+data Expression b where
+    Binding :: Variable b -> Expression a -> Expression b
     Comment :: Rope -> Expression ()
     Declaration :: (a -> b) -> Expression (a -> b)
     Application :: Expression (a -> b) -> Expression a -> Expression b 
     Attribute :: Role -> Expression a -> Expression a
+-}
+
 
 data Type t = Type
     { typeName :: Rope
@@ -30,10 +37,33 @@ data Procedure a b = Expression
     , procedureOutput :: Type b
     , procedureLabel :: Markdown
     , procedureDescription :: Markdown
-    , procedureBody :: Expression b
+    , procedureBlock :: Statement b
     }
 
-evaluate :: Expression a -> a
+data Block b = Block [Statement b]      -- wrong, because they have different types along the way
+
+data Statement b where
+    Binding :: Variable b -> Expression a -> Statement b
+    Comment :: Rope -> Statement ()
+    Attribute :: Role -> Block b -> Statement b
+
+data Expression b where
+    Application :: Expression (a -> b) -> Expression a -> Expression b
+    Value :: Quantity b -> Expression b
+    Declaration :: Procedure a b -> Statement b
+
+
+-- aka apply
+execute :: Procedure a b -> Value a -> b
+execute proc value =
+  let
+    body = procedureBody proc
+  in
+    evaluate body
+
+type Context = Map String Value -- ?
+
+evaluate :: Context -> Expression -> Value
 evaluate e = case e of
     Comment _ -> ()
     _ -> undefined
