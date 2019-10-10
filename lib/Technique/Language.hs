faf76425b6c36a54f12e89799a8611bc0cba6c91
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-10 23:46:28 +1100

    Further refine Values, Variables, and Expressions

--- lib/Technique/Language.hs
+++ lib/Technique/Language.hs
@@ -8,13 +8,24 @@ import Core.Text.Rope
 import Technique.Quantity
 
 -- TODO
-data Variable
-    = Constant
-    | Future
+data Variable = Variable
+    { variableName :: Rope
+    }
 
+-- TODO construction needs to validate internal rules for labels. No
+-- newlines, perhaps.
+type Label = Rope
+
+-- TODO *not* Maybe! If we have two option types here then there are four
+-- possibilities we need to cater for [(Nothing,Nothing), ... (Just,Just)]
+-- which means there are four semantics for a given table row. Hm.
+
+-- TODO there's an ambiguity here between the type of a table and the type
+-- of an individual quantity. Which is "type"?
 data Value = Value
     { valueType :: Type
-    , valueActual :: Maybe String       -- FIXME no wtf
+    , valueLabel :: Maybe Label
+    , valueData :: Maybe Quantity
     }
 
 data Role
@@ -43,25 +54,30 @@ data Procedure = Procedure
     { procedureName :: Rope
     , procedureInput :: Type
     , procedureOutput :: Type
-    , procedureLabel :: Markdown
-    , procedureDescription :: Markdown
+    , procedureLabel :: Maybe Markdown
+    , procedureDescription :: Maybe Markdown
     , procedureBlock :: Block
     }
 
 data Block = Block [Statement]
 
 data Statement where
-    Binding :: Variable -> Expression -> Statement
+    Assignment :: Variable -> Expression -> Statement
+    Execute :: Expression -> Statement
     Comment :: Rope -> Statement
     Declaration :: Procedure -> Statement
-    Attribute :: Role -> Block -> Statement
+    Attribute :: Role -> Block -> Statement     -- Role, Location, and ...?
     Result :: Expression -> Statement
 
 data Expression where
     Application :: Procedure -> Expression -> Expression
-    Quantity :: Quantity -> Expression
+    Literal :: Quantity -> Expression
+    Table :: Tablet -> Expression
+    Binding :: Label -> Expression -> Expression    -- only valid are Literal and Variable?
 
+data Tablet = Tablet [(Label,Quantity)]
 
+{-
 -- aka apply
 execute :: Procedure -> Value -> b
 execute proc value =
@@ -72,9 +88,9 @@ execute proc value =
 
 type Context = Map String Value -- ?
 
+
 evaluate = undefined
 
-{-
 evaluate :: Context -> Expression -> Value
 evaluate e = case e of
     Comment _ -> ()
