c7ebf56f26f2e480f9d74a7f7329f18ba2760f30
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-14 20:13:06 +1100

    Test rendering of a result Tablet
    Add a helper function to render to an expectation in a quasi-quoted here
    document.

--- tests/CheckAbstractSyntax.hs
+++ tests/CheckAbstractSyntax.hs
@@ -6,8 +6,11 @@ module CheckAbstractSyntax
     )
 where
 
+import Core.Data.Structures
 import Core.Text.Rope ()
-import Core.Text.Utilities ()
+import Core.Text.Utilities
+import Data.Maybe (fromJust)
+import Data.Text.Prettyprint.Doc (line)
 import Test.Hspec
 
 import Technique.Language
@@ -15,6 +18,14 @@ import Technique.Quantity
 import Technique.Formatter
 
 import ExampleProcedure hiding (main)
+
+{-|
+When we set the expectation using a [quote| ... |] here doc we get a
+trailing newline that is not present in the rendered AST element. So
+administratively add one here.
+-}
+renderTest :: Render a => a -> String
+renderTest x = show (intoDocA x <> line)
 
 {-|
 These are less tests than a body of code that exercises construction of
@@ -33,4 +44,19 @@ checkAbstractSyntax = do
         it "renders a list as tuple" $ do
             show (commaCat [Name "one", Name "two", Name "three"])
                 `shouldBe` "one,two,three"
+
+        it "renders a tablet as expected" $
+          let
+            hours = fromJust (lookupKeyValue "hr" units)
+            tablet = Tablet
+                        [ Binding "Final temperature" (Variable (Name "temp"))
+                        , Binding "Cooking time" (Grouping (Literal (Quantity 3 hours)))
+                        ]
+          in do
+            renderTest tablet `shouldBe` [quote|
+[
+    "Final temperature" ~ temp
+    "Cooking time" ~ (3 hr)
+]
+|]
 
