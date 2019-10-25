dd1d0c421396f4839374a059b0c56faf3a6ded88
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-25 19:32:04 +1100

    Implement check command

--- src/TechniqueUser.hs
+++ src/TechniqueUser.hs
@@ -9,7 +9,11 @@ module TechniqueUser where
 
 import Core.Program
 import Core.Text
+import Core.System
 import Technique.Procedure ()
+import Technique.Parser
+
+import Text.Megaparsec
 
 commandCheckTechnique :: Program None ()
 commandCheckTechnique = do
@@ -19,7 +23,25 @@ commandCheckTechnique = do
             Nothing -> error "invalid"
             Just file -> file
 
-    writeS procfile
+    event "Read procedure file"
+    contents <- liftIO $ withFile procfile ReadMode hInput
+
+    event "Parse procedure file into Procedure"
+
+    -- This is somewhat horrible; reading into Bytes, then going through
+    -- Rope to get to Text is a bit silly... except that interop was kinda
+    -- the whole point of the unbeliever library. So, good? We can make
+    -- this better if/when we come up with an effecient Stream Rope
+    -- instance so megaparsec can use Rope directly.
+
+    let result = parse pProcedure procfile (fromRope (intoRope contents))
+    case result of
+        Right _ -> do
+            write "Ok"
+        Left err -> do
+            write (intoRope (errorBundlePretty err))
+            terminate 42
+
 
 commandFormatTechnique :: Program None ()
 commandFormatTechnique = do
