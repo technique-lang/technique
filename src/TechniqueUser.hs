0fd5b1219b4ecd7fb913d3861021b58ca7c9ea2c
Andrew Cowie <andrew@operationaldynamics.com>
2019-10-25 22:04:25 +1100

    Watch for changes to procfile when checking

--- src/TechniqueUser.hs
+++ src/TechniqueUser.hs
@@ -7,6 +7,7 @@ Programs implementing front-end commands for users: check, format
 
 module TechniqueUser where
 
+import Control.Monad (forever, void)
 import Core.Program
 import Core.Text
 import Core.System
@@ -15,14 +16,31 @@ import Technique.Parser
 
 import Text.Megaparsec
 
+data Mode = Cycle | Once
+
 commandCheckTechnique :: Program None ()
 commandCheckTechnique = do
     params <- getCommandLine
     
     let procfile = case lookupArgument "filename" params of
-            Nothing -> error "invalid"
-            Just file -> file
+            Just file   -> file
+            _           -> error "Invalid State"
 
+    let mode = case lookupOptionFlag "watch" params of
+            Just True   -> Cycle
+            Nothing     -> Once
+            _           -> error "Invalid State"
+
+    case mode of
+        Once -> do
+            -- normal operation, single pass
+            void (syntaxCheck procfile)
+        Cycle -> do
+            -- use inotify to rebuild on changes
+            forever (syntaxCheck procfile >> waitForChange [procfile])
+
+syntaxCheck :: FilePath -> Program None ()
+syntaxCheck procfile = do
     event "Read procedure file"
     contents <- liftIO $ withFile procfile ReadMode hInput
 
@@ -40,7 +58,6 @@ commandCheckTechnique = do
             write "Ok"
         Left err -> do
             write (intoRope (errorBundlePretty err))
-            terminate 42
 
 
 commandFormatTechnique :: Program None ()
