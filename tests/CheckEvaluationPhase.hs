76b52923388908bb661db248246d12e904e188c8
Andrew Cowie <andrew@operationaldynamics.com>
2020-05-14 22:29:13 +1000

    Format code with Ormolu

--- tests/CheckEvaluationPhase.hs
+++ tests/CheckEvaluationPhase.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 module CheckEvaluationPhase
-    ( checkEvaluationPhase
-    , main
-    )
+  ( checkEvaluationPhase,
+    main,
+  )
 where
 
 import Core.Data.Structures
@@ -13,42 +15,45 @@ import Core.System
 import Core.Text.Rope ()
 import Data.DList (fromList)
 import Data.Int (Int64)
-import Test.Hspec hiding (context)
-
-import Technique.Internal
-import Technique.Evaluator
-import Technique.Quantity
-
 import ExampleProcedure hiding (main)
+import Technique.Evaluator
+import Technique.Internal
+import Technique.Quantity
+import Test.Hspec hiding (context)
 
 main :: IO ()
 main = do
-    finally (hspec checkEvaluationPhase) (putStrLn ".")
+  finally (hspec checkEvaluationPhase) (putStrLn ".")
 
 runProgram :: Program None a -> IO a
 runProgram program = do
-    context <- configure "0" None blank
-    subProgram context program
+  context <- configure "0" None blank
+  subProgram context program
+
+testEvaluation :: Evaluate Value -> IO Value
+testEvaluation = runProgram . runEvaluate testUnique
 
 testUnique :: Unique
 testUnique = emptyUnique
-
 
 exampleSubroutineOven :: Function
 exampleSubroutineOven = Subroutine exampleProcedureOven NoOp
 
 exampleSequence :: Step
-exampleSequence = Nested 0 (fromList
-    [ undefined
-    ])
+exampleSequence =
+  Nested
+    0
+    ( fromList
+        [ undefined
+        ]
+    )
 
 singleStep :: Step
 singleStep = Known 0 (Quanticle (Number 42))
 
-
 checkEvaluationPhase :: Spec
 checkEvaluationPhase = do
-    describe "Evaluator behaviour fundamentals" $ do
-        it "single Step of Known Quantity evaluates" $ do
-            result <- runProgram (runEvaluate testUnique (evaluateStep singleStep))
-            result `shouldBe` Quanticle (Number 42)
+  describe "Evaluator behaviour fundamentals" $ do
+    it "single Step of Known Quantity evaluates" $ do
+      result <- testEvaluation (evaluateStep singleStep)
+      result `shouldBe` Quanticle (Number 42)
