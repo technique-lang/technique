4e25317f888bd015c4a932b7e298705a4000fb36
Andrew Cowie <istathar@gmail.com>
2021-01-17 22:47:35 +1100

    Format code with Fourmolu

--- tests/CheckQuantityBehaviour.hs
+++ tests/CheckQuantityBehaviour.hs
@@ -2,9 +2,9 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module CheckQuantityBehaviour
-  ( checkQuantityBehaviour,
-    main,
-  )
+    ( checkQuantityBehaviour,
+      main,
+    )
 where
 
 import Core.System
@@ -16,25 +16,26 @@ import Test.Hspec
 
 main :: IO ()
 main = do
-  finally (hspec checkQuantityBehaviour) (putStrLn ".")
+    finally (hspec checkQuantityBehaviour) (putStrLn ".")
 
 renderTest :: Render a => a -> String
 renderTest x = show (highlight x)
 
--- |
--- These are less tests than a body of code that exercises construction of
--- an abstract syntax tree.
+{- |
+These are less tests than a body of code that exercises construction of
+an abstract syntax tree.
+-}
 checkQuantityBehaviour :: Spec
 checkQuantityBehaviour = do
-  describe "Basic behaviour of Quantity type" $ do
-    it "Numbers serialze as integers" $ do
-      renderTest (Number 42) `shouldBe` "42"
+    describe "Basic behaviour of Quantity type" $ do
+        it "Numbers serialze as integers" $ do
+            renderTest (Number 42) `shouldBe` "42"
 
-    it "Abbreviated measurement and unit" $ do
-      renderTest (Quantity (Decimal 4 0) (Decimal 0 0) 0 "kg") `shouldBe` "4 kg"
+        it "Abbreviated measurement and unit" $ do
+            renderTest (Quantity (Decimal 4 0) (Decimal 0 0) 0 "kg") `shouldBe` "4 kg"
 
-    it "Full measurement with uncertainty, magnitude, and unit" $ do
-      renderTest (Quantity (Decimal 4 0) (Decimal 1 0) 2 "kg") `shouldBe` "4 ± 1 × 10² kg"
+        it "Full measurement with uncertainty, magnitude, and unit" $ do
+            renderTest (Quantity (Decimal 4 0) (Decimal 1 0) 2 "kg") `shouldBe` "4 ± 1 × 10² kg"
 
-    it "Full measurement with decimal precision" $ do
-      renderTest (Quantity (Decimal 59722 4) (Decimal 6 4) 24 "kg") `shouldBe` "5.9722 ± 0.0006 × 10²⁴ kg"
+        it "Full measurement with decimal precision" $ do
+            renderTest (Quantity (Decimal 59722 4) (Decimal 6 4) 24 "kg") `shouldBe` "5.9722 ± 0.0006 × 10²⁴ kg"
