ca4595c30f3e05f3f25766499fa00cc54c546903
Andrew Cowie <istathar@gmail.com>
2021-01-17 23:07:20 +1100

    Format code with Fourmolu

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -9,11 +9,11 @@ import Test.Hspec (Spec, hspec)
 
 main :: IO ()
 main = do
-  finally (hspec suite) (putStrLn ".")
+    finally (hspec suite) (putStrLn ".")
 
 suite :: Spec
 suite = do
-  checkQuantityBehaviour
-  checkConcreteSyntax
-  checkSkeletonParser
-  checkTranslationPhase
+    checkQuantityBehaviour
+    checkConcreteSyntax
+    checkSkeletonParser
+    checkTranslationPhase
