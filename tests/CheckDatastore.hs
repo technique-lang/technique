bb00ef83c420c85e8c9f9a2921f626a06faf56cb
Andrew Cowie <istathar@gmail.com>
2021-01-26 16:13:40 +1100

    Format code with Fourmolu

--- tests/CheckDatastore.hs
+++ tests/CheckDatastore.hs
@@ -12,37 +12,37 @@ import Prelude hiding (catch)
 
 spec :: Spec
 spec = do
-  describe "Redis backend" $ do
-    testStoreKey
-    testReadKey
-    testNonexistentKey
+    describe "Redis backend" $ do
+        testStoreKey
+        testReadKey
+        testNonexistentKey
 
 answer' :: ByteString
 answer' = "Life, universe, and everything"
 
 testStoreKey =
-  it "stores a value at a given key" $ do
-    storeResource k' v'
-    assertBool "" True -- it didn't throw an exception; good
+    it "stores a value at a given key" $ do
+        storeResource k' v'
+        assertBool "" True -- it didn't throw an exception; good
   where
     k' = "42"
     v' = answer'
 
 testReadKey =
-  it "reads that key and gets the same value back" $ do
-    res'0 <- lookupResource k
-    assertMaybe "no value for this key" res'0
-    let res' = fromJust res'0
-    assertEqual "wrong value returned" v' res'
+    it "reads that key and gets the same value back" $ do
+        res'0 <- lookupResource k
+        assertMaybe "no value for this key" res'0
+        let res' = fromJust res'0
+        assertEqual "wrong value returned" v' res'
   where
     k = "42"
     v' = answer'
 
 testNonexistentKey =
-  it "handles non-existent keys" $ do
-    res'0 <- lookupResource k
-    case res'0 of
-      Just _ -> assertFailure "Non-existent key; should not have a value!"
-      Nothing -> return ()
+    it "handles non-existent keys" $ do
+        res'0 <- lookupResource k
+        case res'0 of
+            Just _ -> assertFailure "Non-existent key; should not have a value!"
+            Nothing -> return ()
   where
     k = "a9s1t$y9e"
