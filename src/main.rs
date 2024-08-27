e820f13177e08eb97d24a81cb3628bed78327ed5
Andrew Cowie <istathar@gmail.com>
2024-08-27 20:40:03 +1000

    Port top level argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("technique")
+        .version("1.0")
+        .propagate_version(true)
+        .author("Andrew Cowie")
+        .about("The Technique Procedures Language.")
+        .disable_help_subcommand(true)
+        .subcommand(
+            Command::new("check")
+                .about("Syntax- and type-check the given procedure")
+                .arg(
+                    Arg::new("watch")
+                        .long("watch")
+                        .help("Watch the given procedure file and recompile if changes are detected."),
+                )
+                .arg(
+                    Arg::new("filename")
+                        .required(true)
+                        .help("The file containing the code for the procedure you want to type-check."),
+                ),
+        )
+        .subcommand(
+            Command::new("format")
+                .about("Code format the given procedure")
+                .arg(
+                    Arg::new("raw-control-chars")
+                        .short('R')
+                        .long("raw-control-chars")
+                        .help("Emit ANSI escape codes for syntax highlighting even if output is redirected to a pipe or file."),
+                )
+                .arg(
+                    Arg::new("filename")
+                        .required(true)
+                        .help("The file containing the code for the procedure you want to format."),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("check", submatches)) => {
+            if submatches.contains_id("watch") {
+                println!("Check command executed with watch option");
+            }
+            if let Some(filename) = submatches.get_one::<String>("filename") {
+                println!("Check command executed with filename: {}", filename);
+            }
+        }
+        Some(("format", submatches)) => {
+            if submatches.contains_id("raw-control-chars") {
+                println!("Format command executed with raw-control-chars option");
+            }
+            if let Some(filename) = submatches.get_one::<String>("filename") {
+                println!("Format command executed with filename: {}", filename);
+            }
+        }
+        _ => println!("No valid subcommand was used"),
+    }
 }
