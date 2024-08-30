b157f40f390bb16b5cf30e6fadc311d55fa90dc9
Andrew Cowie <istathar@gmail.com>
2024-08-30 15:36:14 +1000

    Change to using tinytemplate for string substitution

--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,20 +1,38 @@
-use std::fs;
+use serde::Serialize;
 use std::io::Write;
-use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 use std::process::{Command, Stdio};
+use tinytemplate::TinyTemplate;
 use tracing::{debug, info};
+
+static TEMPLATE: &'static str = r#"
+#show raw: set text(font: "Inconsolata")
+#show raw.where(lang: "technique"): set raw(
+        lang: "technique",
+        syntaxes: "technique.sublime-syntax",
+        theme: "technique.tmTheme",
+    )
+
+#show raw: set block(breakable: true)
+#raw(
+        block: true,
+        lang: "technique",
+        read("{filename}")
+    )
+"#;
+
+#[derive(Serialize)]
+struct Context {
+    filename: String,
+}
 
 pub(crate) fn via_typst(filename: &Path) {
     info!("Printing file: {:?}", filename);
 
     // Verify that the file actually exists
     if !filename.exists() {
-        panic!("Supplied procedure file does not exist: {:?}", filename);
+        panic!("Supplied procedure file does not exist: {}", filename.display());
     }
-
-    // Read the file contents
-    let contents = fs::read_to_string(filename).expect("Unable to read specified file");
 
     let mut child = Command::new("typst")
         .arg("compile")
@@ -27,38 +45,17 @@ pub(crate) fn via_typst(filename: &Path) {
     // Write the file contents to the process's stdin
     let mut stdin = child.stdin.take().unwrap();
 
+    let mut tt = TinyTemplate::new();
+    tt.add_template("hello", TEMPLATE).unwrap();
+
+    let context = Context {
+        filename: filename.display().to_string(),
+    };
+
+    let rendered = tt.render("hello", &context).unwrap();
     stdin
-        .write(
-            format!(
-                r#"
-#show raw: set text(font: "Inconsolata")
-#show raw.where(lang: "technique"): set raw(
-        lang: "technique",
-        syntaxes: "technique.sublime-syntax",
-        theme: "technique.tmTheme",
-    )
-
-#show raw: set block(breakable: true)
-#raw(
-        block: true,
-        lang: "technique",
-        read("{}")
-    )"#,
-                filename.to_str().expect("Should be able to render filename")
-            )
-            .as_bytes(),
-        )
+        .write(rendered.as_bytes())
         .expect("Write header to child prcess");
-
-    // stdin
-    //     .write_all(contents.as_bytes())
-    //     .expect("Failed to write to stdin");
-
-    //     stdin.write(filename.as_os_str().as_bytes()).expect("Write filename to child process");
-
-    //     stdin.write(r#"")
-    //     )
-    // "#.as_bytes()).expect("Write tail to child process");
 
     drop(stdin);
 
