1e440ea01c771661642976a1e9530145e82b658f
Andrew Cowie <istathar@gmail.com>
2024-09-16 13:21:43 +1000

    Grammar for procedure declaration

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -1,4 +1,36 @@
 // parsing machinery
 
-pub fn load() {
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "../technique.pest"]
+struct TechniqueParser;
+
+pub fn load() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // Import all parent module items
+
+    #[test]
+    fn check_procedure_declaration() {
+        let input = "making_coffee : Beans -> Coffee";
+
+        let declaration = TechniqueParser::parse(Rule::declaration, &input)
+            .expect("Unsuccessful Parse")
+            .next()
+            .unwrap();
+
+        assert_eq!(declaration.as_str(), "making_coffee : Beans -> Coffee");
+        assert_eq!(declaration.as_rule(), Rule::declaration);
+
+        let identifier = declaration
+            .into_inner()
+            .next()
+            .unwrap();
+
+        assert_eq!(identifier.as_str(), "making_coffee");
+        assert_eq!(identifier.as_rule(), Rule::identifier);
+    }
 }
