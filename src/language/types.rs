a4d2acef9dbd42d2220d8008946801eea971ae1c
Andrew Cowie <istathar@gmail.com>
2024-11-30 14:59:38 +1100

    Add tests to language AST types

--- src/language/types.rs
+++ src/language/types.rs
@@ -47,3 +47,51 @@ pub struct Signature<'i> {
 pub struct Attribute<'i> {
     pub name: &'i str,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn maker<'i>() -> Technique<'i> {
+        let t1 = Technique {
+            version: 1,
+            license: None,
+            copyright: None,
+            template: None,
+        };
+
+        t1
+    }
+
+    #[test]
+    fn check_ast_construction() {
+        let t1 = Technique {
+            version: 1,
+            license: None,
+            copyright: None,
+            template: None,
+        };
+
+        assert_eq!(Technique::default(), t1);
+
+        let t2 = Technique {
+            version: 1,
+            license: Some("MIT"),
+            copyright: Some("ACME, Inc"),
+            template: Some("checklist"),
+        };
+
+        let t3 = maker();
+
+        assert_eq!(t3, t1);
+
+        let t4 = Technique {
+            license: Some("MIT"),
+            copyright: Some("ACME, Inc"),
+            template: Some("checklist"),
+            ..t3
+        };
+
+        assert_eq!(t4, t2);
+    }
+}
