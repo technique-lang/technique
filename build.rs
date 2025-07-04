ec8570ac1c99cc539257231f28fcba93af6fb9e2
Andrew Cowie <istathar@gmail.com>
2025-07-04 12:47:06 +1000

    Add machinery to compile parser on build

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut config = cc::Build::new();
+
+    // Compile the generated C parser
+    config.file("src/parser.c");
+    
+    // Also include the directory with the C headers
+    config.include("src");
+
+    config.compile("tree-sitter-example");
+
+    // Rerun the build script if the C source changes
+    println!("cargo:rerun-if-changed=src/parser.c");
+}
