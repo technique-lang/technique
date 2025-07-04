b35c1ba5dbfe0c796ea086e5662f9c6d991ab959
Andrew Cowie <istathar@gmail.com>
2025-07-04 13:53:44 +1000

    Add generated Tree Sitter parser

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ fn main() {
 
     // Compile the generated C parser
     config.file("src/parser.c");
-    
+
     // Also include the directory with the C headers
     config.include("src");
 
-    config.compile("tree-sitter-example");
+    config.compile("tree-sitter-technique");
 
     // Rerun the build script if the C source changes
     println!("cargo:rerun-if-changed=src/parser.c");
--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -3,10 +3,16 @@ use std::path::Path;
 
 mod parser;
 mod scope;
+mod tree_sitter;
 
 pub fn load(source: &Path) {
     // read source to a str
     let content = std::fs::read_to_string(source).expect("Failed to read the source file");
 
-    parser::parse_via_scopes(content.as_str());
+    let tree = tree_sitter::parse_tree(content.as_str());
+    println!(
+        "{}",
+        tree.root_node()
+            .to_sexp()
+    );
 }
